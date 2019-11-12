070b8e00db7ace68b1cf7c7ba9a48366570f7850
Neah Nieto <nrpcnieto@gmail.com>
2019-11-11 19:49:00 -0600

    Forgot to add .save method to update action in controller

--- app/controllers/api/contacts_controller.rb
+++ app/controllers/api/contacts_controller.rb
@@ -25,6 +25,7 @@ class Api::ContactsController < ApplicationController
     @contact.last_name = params[:last_name] || @contact.last_name
     @contact.email = params[:email] || @contact.email
     @contact.phone_number = params[:phone_number] || @contact.phone_number
+    @contact.save
     render "show.json.jb"
   end
 
