e5395b9410a3f8908a6eafd0643bd14509a1f53f
Neah Nieto <nrpcnieto@gmail.com>
2019-11-13 18:52:04 -0600

    Added initial data for seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+contact = ContactList.new(
+  first_name: "Bilbo",
+  middle_name: "Took",
+  last_name: "Baggins",
+  bio: "Respectable hobbit from the Shire",
+  email: "bilbo@shire.org",
+  phone_number: "111-111-1111",
+)
+contact.save
+contact = ContactList.new(
+  first_name: "James",
+  middle_name: "Tiberius",
+  last_name: "Kirk",
+  bio: "Captain of the Federation Starship Enterprise",
+  email: "jtkirk@federation.org",
+  phone_number: "211-919-1701",
+)
+contact.save
+contact = ContactList.new(
+  first_name: "Jon",
+  middle_name: "Rhaegar",
+  last_name: "Snow",
+  bio: "Lord Commander of the Night's Watch",
+  email: "lordcommander@nightswatch.org",
+  phone_number: "998-300-8800",
+)
+contact.save
