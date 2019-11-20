6a616340617bf4dad080dbdfed5125ce9aaf72b5
Neah Nieto <nrpcnieto@gmail.com>
2019-11-19 18:43:06 -0600

    Add validations for User model in app/model/users.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_13_022311) do
+ActiveRecord::Schema.define(version: 2019_11_20_004131) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,14 @@ ActiveRecord::Schema.define(version: 2019_11_13_022311) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "middle_name"
     t.text "bio"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
